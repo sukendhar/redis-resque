b07bcab8919eb98f4caa1c35a859b8a3bb81b4bd
sukendhar <sukendhar555@gmail.com>
2018-04-16 22:09:27 +0530

    added resque

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'resque/server'
+
 Rails.application.routes.draw do
   resources :posts
+  mount Resque::Server.new, at: "/resque"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
